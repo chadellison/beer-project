1655644699d9cf51db3b1b0851553a6cebce517d
Chad Ellison <chad.ellison0123@gmail.com>
2016-12-17 18:44:32 -0700

    Event listeners

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import '../App.css';
 
 export default class Nav extends Component {
+  handleDropdown() {
+    alert("this is the dropdown")
+  }
+
   render() {
     return (
       <ul className="nav-bar">
-        <li><a href="#">beer type</a></li>
-        <li><a href="#">sort beers</a></li>
-        <li><a href="#">add a beer</a></li>
+        <li><a href="#" onClick={this.handleDropdown}>beer type</a></li>
+        <li><a href="#" onClick={this.handleDropdown}>sort beers</a></li>
+        <li><a href="#" onClick={this.handleDropdown}>add a beer</a></li>
         <li><input placeholder="search beers"></input></li>
       </ul>
     )
