44bf068f7a27689997ce8c6f5c9f522d40036a8b
Chad Ellison <chad.ellison0123@gmail.com>
2016-12-17 22:10:05 -0700

    improve styling

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -2,28 +2,6 @@ import React, { Component } from 'react';
 import '../App.css';
 
 export default class Beers extends Component {
-  // <li>bee2222r 2</li>
-  // <li>beeeeer 3</li>
-  // <li>beeer 4</li>
-  // <li>beeeeeer 5</li>
-  // <li>beer 6</li>
-  // <li>beeeeeeer 7</li>
-  // <li>beer 8</li>
-  // <li>beer 9</li>
-  // <li>beer 10</li>
-  // <li>beer 11</li>
-  // <li>beer 12</li>
-  // <li>beer 13</li>
-  // <li>beer 14</li>
-  // <li>bee3eeeer 15</li>
-  // <li>b3333333r 16</li>
-  // <li>beer 17</li>
-  // <li>beer 18</li>
-  // <li>beer 19</li>
-  // <li>beer 20</li>
-  // <li>beer 21</li>
-  // <li>beer 22</li>
-  // <li>superb beer 23</li>
   rateBeer() {
     alert("rate this beer")
   }
@@ -35,7 +13,7 @@ export default class Beers extends Component {
         <div className="beer-name"><strong>Name:</strong> {name}</div>
         <div className="beer-type"><strong>Type:</strong> {type}</div>
         <div className="beer-rating"><strong>Rating:</strong> {rating}</div>
-        <button className="rate-beer" onClick={this.rateBeer}>{"Rate this beer"}</button>
+        <button className="rate-beer" onClick={this.rateBeer}>{"Rate"}</button>
       </div>
     )
   }
@@ -46,6 +24,32 @@ export default class Beers extends Component {
         <li>{this.singleBeer("url", "down town brown", 4, "Brown ale")}</li>
         <li>{this.singleBeer("url", "chocolate delight", 3.7, "Porter")}</li>
         <li>{this.singleBeer("url", "coffee stout", 4.2, "Stout")}</li>
+        <li>{this.singleBeer("url", "1620", 3.2, "Lager")}</li>
+        <li>{this.singleBeer("url", "Renegade Blues", 4.6, "IPA")}</li>
+        <li>{this.singleBeer("url", "Bomber", 2.2, "Lager")}</li>
+        <li>{this.singleBeer("url", "Three philosophers", 3.2, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
+        <li>{this.singleBeer("url", "The Reverence", 4.8, "Belgian")}</li>
       </ul>
     )
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
+import beer from '../beer.png';
 import '../App.css';
 
 export default class Header extends Component {
   render() {
     return (
       <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to the Beer Project</h2>
+        <img src={beer} className="beer-profile" alt="beer" />
+        <h2>The Beer Project</h2>
       </div>
     )
   }
