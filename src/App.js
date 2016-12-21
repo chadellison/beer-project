00d8e788bbc65a1c788efe48fe2f68c09eeefb11
Chad Ellison <chad.ellison0123@gmail.com>
2016-12-21 16:58:25 -0700

    Break out css classes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './header.css';
+import './nav.css';
+import './footer.css';
+import './body.css';
 import Header from './components/Header.js';
 import Intro from './components/Intro.js';
 import Beers from './components/Beers.js';
 import Nav from './components/Nav.js';
 import Footer from './components/Footer.js';
-import Dropdown from './components/dropdown.js';
-
 
 class App extends Component {
   render() {
