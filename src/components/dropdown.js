638a79ad3b7a95170987121aaaf43efc20e5da0a
Chad Ellison <chad.ellison0123@gmail.com>
2016-12-28 15:27:20 -0700

    change dropdown to reflect selected value

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -5,8 +5,13 @@ export default class Dropdown extends Component {
   constructor(props) {
     super(props);
     this.toggleMenu = this.toggleMenu.bind(this);
+    this.selectIpa = this.selectIpa.bind(this);
+    this.selectStout = this.selectStout.bind(this);
+    this.selectLager = this.selectLager.bind(this);
+    this.selectBelgian = this.selectBelgian.bind(this);
     this.state = {
-      menuActive: false
+      menuActive: false,
+      beerType: "All Beers"
     };
   }
 
@@ -17,15 +22,39 @@ export default class Dropdown extends Component {
     });
   }
 
+  selectIpa() {
+    this.setState({
+      beerType: "IPAs"
+    })
+  }
+
+  selectStout() {
+    this.setState({
+      beerType: "Stouts"
+    })
+  }
+
+  selectLager() {
+    this.setState({
+      beerType: "Lagers"
+    })
+  }
+
+  selectBelgian() {
+    this.setState({
+      beerType: "Belgians"
+    })
+  }
+
   render() {
     let menu;
     if(this.state.menuActive) {
       menu = <div className="menu">
                <ul>
-                 <li>IPAs</li>
-                 <li>Stouts</li>
-                 <li>Lagers</li>
-                 <li>Belgiansssssssss</li>
+                 <li onClick = { this.selectIpa }>IPAs</li>
+                 <li onClick = { this.selectStout }>Stouts</li>
+                 <li onClick = { this.selectLager }>Lagers</li>
+                 <li onClick = { this.selectBelgian }>Belgiansssssssss</li>
                </ul>
              </div>
     } else {
@@ -33,7 +62,7 @@ export default class Dropdown extends Component {
     }
     return (
       <li onClick = { this.toggleMenu } >
-        beer type
+        {this.state.beerType}
         {menu}
       </li>
     )
