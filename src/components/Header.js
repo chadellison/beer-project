dd5794ee67940dd586402348a57353fce304eb47
Chad Ellison <chad.ellison0123@gmail.com>
2017-02-18 01:11:20 -0700

    make app responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ export default class Header extends Component {
     return (
       <div className="appHeader">
         <img src={beer} className="beerProfile" alt="beer" />
-        <h2>The Beer Project</h2>
+        <h2 className="title">The Beer Project</h2>
       </div>
     )
   }
