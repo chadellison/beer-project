d30d9a14d9a4fa6a862e7f61181fe26b11a6279f
Chad Ellison <chad.ellison0123@gmail.com>
2017-03-01 16:45:35 -0700

    add not yet rated

--- src/components/SingleBeer.js
+++ src/components/SingleBeer.js
@@ -6,6 +6,7 @@ export default class SingleBeer extends Component {
   constructor(props) {
     super(props)
     this.toggleRateBeer = this.toggleRateBeer.bind(this)
+    this.displayRating  = this.displayRating.bind(this)
     this.state = {
       rateBeerMenu: false
     }
@@ -15,6 +16,14 @@ export default class SingleBeer extends Component {
     this.setState({
       rateBeerMenu: !this.state.rateBeerMenu
     })
+  }
+
+  displayRating() {
+    if(this.props.rating == "0.0") {
+      return "not yet rated"
+    } else {
+      return this.props.rating
+    }
   }
 
   render() {
@@ -34,7 +43,7 @@ export default class SingleBeer extends Component {
         <img className="beerImage" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTLgFs_rSTVl48wtFvqrloD_Ci1mLQRz_B9WExrrUnPCdjbD81Lhg"></img>
         <div className="beerName"><strong>Name:</strong> {this.props.name}</div>
         <div className="beerType"><strong>Type:</strong> {this.props.type}</div>
-        <div className="beerRating"><strong>Rating:</strong> {this.props.rating}</div>
+        <div className="beerRating"><strong>Rating:</strong> {this.displayRating()}</div>
         <button className="rateBeer other" onClick={this.toggleRateBeer}>Rate Beer</button>
         {rateBeer}
       </div>
