606f838a6028fd4c04173578b283326c460335bb
Chad Ellison <chad.ellison0123@gmail.com>
2017-03-03 02:03:27 -0700

    adjust type in config

--- src/config/Hosts.js
+++ src/config/Hosts.js
@@ -3,7 +3,7 @@ export default class Hosts {
     if(window.location.hostname === "localhost") {
       return "http://localhost:3001"
     } else {
-      return "https://http://beer-server0123.herokuapp.com"
+      return "https://beer-server0123.herokuapp.com"
     }
   }
 }
