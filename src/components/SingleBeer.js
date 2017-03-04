372cb97b5e13caede1bcf90b010bd98f4b3603b9
Chad Ellison <chad.ellison0123@gmail.com>
2017-03-04 15:33:15 -0700

    display abv

--- src/components/SingleBeer.js
+++ src/components/SingleBeer.js
@@ -6,6 +6,8 @@ export default class SingleBeer extends Component {
     super(props)
     this.toggleRateBeer = this.toggleRateBeer.bind(this)
     this.displayRating  = this.displayRating.bind(this)
+    this.displayAbv     = this.displayAbv.bind(this)
+    this.displayText    = this.displayText.bind(this)
     this.state = {
       rateBeerMenu: false
     }
@@ -17,11 +19,21 @@ export default class SingleBeer extends Component {
     })
   }
 
+  displayText(field) {
+    return field.charAt(0).toUpperCase() + field.slice(1)
+  }
+
   displayRating() {
-    if(this.props.rating == "0.0") {
+    if(this.props.rating === "0.0") {
       return "not yet rated"
     } else {
       return this.props.rating
+    }
+  }
+
+  displayAbv() {
+    if(this.props.abv) {
+      this.props.abv + " %"
     }
   }
 
@@ -41,8 +53,9 @@ export default class SingleBeer extends Component {
     return (
       <div className="beer">
         <img className="beerImage" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTLgFs_rSTVl48wtFvqrloD_Ci1mLQRz_B9WExrrUnPCdjbD81Lhg"></img>
-        <div className="beerName"><strong>Name:</strong> {this.props.name}</div>
-        <div className="beerType"><strong>Type:</strong> {this.props.type}</div>
+        <div className="beerName"><strong>Name:</strong> {this.displayText(this.props.name)}</div>
+        <div className="beerType"><strong>Type:</strong> {this.displayText(this.props.type)}</div>
+        <div className="abv"><strong>ABV:</strong> {this.displayAbv}</div>
         <div className="beerRating"><strong>Rating:</strong> {this.displayRating()}</div>
         <button className="rateBeer other" onClick={this.toggleRateBeer}>Rate Beer</button>
         {rateBeer}
