f2e263a55470fe7f5dcf28e502380ccf0c0336e4
Chad Ellison <chad.ellison0123@gmail.com>
2017-03-05 15:08:11 -0700

    add contact mailer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,16 @@ import twitter from "../../public/twitter.png"
 import instagram from "../../public/instagram.png"
 
 export default class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.handleContact = this.handleContact.bind(this)
+  }
+
+  handleContact() {
+    window.location.href = "mailto:beer.project0123@gmail.com" +
+    "?subject=Contact&body=message%20goes%20here"
+  }
+
   render() {
     return (
       <div className="appFooter">
@@ -13,7 +23,7 @@ export default class Header extends Component {
           <img src={twitter} className="twitter" alt="twitter icon" />
           <img src={instagram} className="instagram" alt="instagram icon" />
         </div>
-        <div className="contact">
+        <div className="contact" onClick={this.handleContact}>
           Contact
         </div>
       </div>
